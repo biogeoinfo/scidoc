99a0130678b5ca3d66c8606dfd59d3346ca2e1d4
Alexey Borisov <baswatch@gmail.com>
2025-08-22 08:55:36 +0700

    Update quartz.config.ts
    [[odonatanote.ru]]

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "scidoc.ru",
+    baseUrl: "odonatanote.ru",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
